e812fbf88ff0d6d73849be3efad795c0143f9953
Douglas Costa <douglascost47@gmail.com>
2020-03-01 19:54:42 -0300

    sem massagem

--- api_projetos_rest/api_projetos_rest/wsgi.py
+++ api_projetos_rest/api_projetos_rest/wsgi.py
@@ -6,7 +6,7 @@ import sys
  
 from django.core.wsgi import get_wsgi_application 
  
-sys.path.append('/var/www/api') 
+sys.path.append('/var/www/api/api_projetos_rest') 
 # adjust the Python version in the line below as needed 
 sys.path.append('/var/www/api/venv/lib/python3.7/site-packages') 
  
